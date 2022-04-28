4466c2ea96e6f1862e82cbdc32ca3af515c04c00
Dmitry Zinkevich <mr.zinch@protonmail.com>
2022-04-28 21:31:51 +0300

    Generate PPM body

--- core/src/main/java/raytracer/core/export/CanvasToPpmConverter.java
+++ core/src/main/java/raytracer/core/export/CanvasToPpmConverter.java
@@ -1,6 +1,10 @@
 package raytracer.core.export;
 
 import raytracer.core.graphics.Canvas;
+import raytracer.core.graphics.Color;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CanvasToPpmConverter {
     private CanvasToPpmConverter() {
@@ -10,7 +14,25 @@ public class CanvasToPpmConverter {
         var newLine = System.getProperty("line.separator");
         var sb = new StringBuilder("P3").append(newLine)
                 .append(c.width).append(" ").append(c.height).append(newLine)
-                .append("255");
+                .append("255").append(newLine);
+        for (int j = 0; j < c.height; j++) {
+            final int y = j;
+            var canvasLine = IntStream.range(0, c.width)
+                    .mapToObj(x -> c.pixelAt(x, y))
+                    .map(CanvasToPpmConverter::rgb)
+                    .collect(Collectors.joining(" "));
+            sb.append(canvasLine).append(newLine);
+        }
         return sb.toString();
+    }
+
+    private static String rgb(Color c) {
+        return normalize(c.red()) + " " + normalize(c.green()) + " " + normalize(c.blue());
+    }
+
+    private static int normalize(double originalValue) {
+        int value = (int) Math.ceil(originalValue * 255);
+        if (value > 255) return 255;
+        return Math.max(value, 0);
     }
 }
--- core/src/test/java/raytracer/core/export/CanvasToPpmConverterTest.java
+++ core/src/test/java/raytracer/core/export/CanvasToPpmConverterTest.java
@@ -1,18 +1,38 @@
 package raytracer.core.export;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import raytracer.core.graphics.Canvas;
+import raytracer.core.graphics.Color;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CanvasToPpmConverterTest {
+    private Canvas canvas;
+
+    @BeforeEach
+    void setUp() {
+        canvas = new Canvas(5, 3);
+    }
+
     @Test
     void constructing_ppm_header() {
-        var canvas = new Canvas(5, 3);
         var ppm = CanvasToPpmConverter.convert(canvas);
         var tokens = ppm.split("\n");
         assertThat(tokens[0]).isEqualTo("P3");
         assertThat(tokens[1]).isEqualTo("5 3");
         assertThat(tokens[2]).isEqualTo("255");
+    }
+
+    @Test
+    void constructing_ppm_pixel_data() {
+        canvas.writePixelAt(new Color(1.5, 0, 0), 0, 0);
+        canvas.writePixelAt(new Color(0, 0.5, 0), 2, 1);
+        canvas.writePixelAt(new Color(-0.5, 0, 1), 4, 2);
+        var ppm = CanvasToPpmConverter.convert(canvas);
+        var tokens = ppm.split("\n");
+        assertThat(tokens[3]).isEqualTo("255 0 0 0 0 0 0 0 0 0 0 0 0 0 0");
+        assertThat(tokens[4]).isEqualTo("0 0 0 0 0 0 0 128 0 0 0 0 0 0 0");
+        assertThat(tokens[5]).isEqualTo("0 0 0 0 0 0 0 0 0 0 0 0 0 0 255");
     }
 }
