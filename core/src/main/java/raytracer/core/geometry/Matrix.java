0a2e6e340959f0ac4c98c5c98dc0f23dbebddfa7
Dmitry Zinkevich <mr.zinch@protonmail.com>
2022-04-30 16:14:49 +0300

    Test creating 2x2 and 3x3 matrices

--- core/src/main/java/raytracer/core/geometry/Matrix.java
+++ core/src/main/java/raytracer/core/geometry/Matrix.java
@@ -8,6 +8,9 @@ public final class Matrix {
     public Matrix(double[][] values) {
         var M = values.length;
         var N = values[0].length;
+        if (N != M) {
+            throw new IllegalArgumentException("Must provide a square matrix!");
+        }
         this.values = new double[N][M];
         for (int i = 0; i < N; i++) {
             this.values[i] = Arrays.copyOf(values[i], N);
--- core/src/test/java/raytracer/core/geometry/MatrixTest.java
+++ core/src/test/java/raytracer/core/geometry/MatrixTest.java
@@ -1,13 +1,13 @@
 package raytracer.core.geometry;
 
 import org.junit.jupiter.api.Test;
-import raytracer.core.geometry.Matrix;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MatrixTest {
     @Test
-    void constructing_a_4x4_matrix() {
+    void constructing_4x4_matrix() {
         double[][] values = {
                 {1, 2, 3, 4},
                 {5.5, 6.5, 7.5, 8.5},
@@ -24,6 +24,39 @@ class MatrixTest {
         assertThat(matrix.at(3, 0)).isEqualTo(13.5);
         assertThat(matrix.at(3, 2)).isEqualTo(15.5);
         assertThat(matrix.at(3, 3)).isEqualTo(16.5);
+    }
 
+    @Test
+    void constructing_2x2_matrix() {
+        var matrix = new Matrix(new double[][]{
+                {-3, 5},
+                {1, -2}
+        });
+        assertThat(matrix.at(0, 0)).isEqualTo(-3);
+        assertThat(matrix.at(0, 1)).isEqualTo(5);
+        assertThat(matrix.at(1, 0)).isEqualTo(1);
+        assertThat(matrix.at(1, 1)).isEqualTo(-2);
+    }
+
+    @Test
+    void constructing_3x3_matrix() {
+        var matrix = new Matrix(new double[][]{
+                {-3, 5, 0},
+                {1, -2, -7},
+                {0, 1, -1}
+        });
+        assertThat(matrix.at(0, 0)).isEqualTo(-3);
+        assertThat(matrix.at(1, 1)).isEqualTo(-2);
+        assertThat(matrix.at(2, 2)).isEqualTo(-1);
+    }
+
+    @Test
+    void allow_only_square_matrices() {
+        assertThatThrownBy(() ->
+                new Matrix(new double[][]{
+                        {1, 2},
+                        {3, 4},
+                        {5, 6}
+                })).isInstanceOf(IllegalArgumentException.class).hasMessage("Must provide a square matrix!");
     }
 }
