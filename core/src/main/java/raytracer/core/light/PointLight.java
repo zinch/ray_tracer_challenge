88475bf79500074a8e3ef9177770d3c2af56a61b
Dmitry Zinkevich <mr.zinch@protonmail.com>
2022-05-05 19:47:54 +0300

    Refactor lightning calculation

--- core/src/main/java/raytracer/core/light/PointLight.java
+++ core/src/main/java/raytracer/core/light/PointLight.java
@@ -1,7 +1,26 @@
 package raytracer.core.light;
 
 import raytracer.core.geometry.Point;
+import raytracer.core.geometry.Vector;
 import raytracer.core.graphics.Color;
 
 public record PointLight(Point position, Color intensity) {
+    public Color lightningAt(Point point, Material material, Vector normal, Vector eye) {
+        var effectiveColor = intensity.times(material.color());
+        var lightVector = position.minus(point).normalize();
+        var ambient = effectiveColor.times(material.ambient());
+        Color diffuse = Color.BLACK;
+        Color specular = Color.BLACK;
+
+        var lightDotNormal = lightVector.dot(normal);
+        if (lightDotNormal >= 0) {
+            diffuse = effectiveColor.times(material.diffuse()).times(lightDotNormal);
+            var reflectDotEye = eye.dot(lightVector.negate().reflect(normal));
+            if (reflectDotEye >= 0) {
+                var factor = Math.pow(reflectDotEye, material.shininess());
+                specular = intensity.times(material.specular()).times(factor);
+            }
+        }
+        return ambient.plus(diffuse).plus(specular);
+    }
 }
--- core/src/test/java/raytracer/core/light/PointLightTest.java
+++ core/src/test/java/raytracer/core/light/PointLightTest.java
@@ -1,10 +1,14 @@
 package raytracer.core.light;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import raytracer.core.geometry.Point;
 import raytracer.core.graphics.Color;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static raytracer.core.geometry.Tuple.point;
+import static raytracer.core.geometry.Tuple.vector;
 
 class PointLightTest {
     @Test
@@ -16,5 +20,61 @@ class PointLightTest {
 
         assertThat(light.position()).isEqualTo(point(0, 0, 0));
         assertThat(light.intensity()).isEqualTo(Color.WHITE);
+    }
+
+    @DisplayName("Lightning")
+    @Nested
+    class Lightning {
+        private final Material material = Material.DEFAULT;
+        private final Point position = point(0, 0, 0);
+        private final PointLight light = new PointLight(point(0, 0, -10), Color.WHITE);
+
+        @Test
+        void lighting_with_the_eye_between_the_light_and_the_surface() {
+            var eye = vector(0, 0, -1);
+            var normal = vector(0, 0, -1);
+
+            assertThat(light.lightningAt(position, material, normal, eye))
+                    .isEqualTo(new Color(1.9, 1.9, 1.9));
+        }
+
+        @Test
+        void lighting_with_the_eye_between_light_and_surface_and_eye_offset_45_deg() {
+            var eye = vector(0, Math.sqrt(2) / 2, -Math.sqrt(2) / 2);
+            var normal = vector(0, 0, -1);
+
+            assertThat(light.lightningAt(position, material, normal, eye))
+                    .isEqualTo(new Color(1.0, 1.0, 1.0));
+        }
+
+        @Test
+        void lightning_with_the_eye_opposite_surface_and_light_offset_45_deg() {
+            var eye = vector(0, 0, -1);
+            var normal = vector(0, 0, -1);
+            var light = new PointLight(point(0, 10, -10), Color.WHITE);
+
+            assertThat(light.lightningAt(position, material, normal, eye))
+                    .isEqualTo(new Color(0.7364, 0.7364, 0.7364));
+        }
+
+        @Test
+        void lightning_with_eye_in_the_path_of_the_reflection_vector() {
+            var eye = vector(0, -Math.sqrt(2) / 2, -Math.sqrt(2) / 2);
+            var normal = vector(0, 0, -1);
+            var light = new PointLight(point(0, 10, -10), Color.WHITE);
+
+            assertThat(light.lightningAt(position, material, normal, eye))
+                    .isEqualTo(new Color(1.6364, 1.6364, 1.6364));
+        }
+
+        @Test
+        void lighting_with_the_light_behind_the_surface() {
+            var eye = vector(0, 0, -1);
+            var normal = vector(0, 0, -1);
+            var light = new PointLight(point(0, 0, 10), Color.WHITE);
+
+            assertThat(light.lightningAt(position, material, normal, eye))
+                    .isEqualTo(new Color(0.1, 0.1, 0.1));
+        }
     }
 }
